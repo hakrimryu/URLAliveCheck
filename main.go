d779a489fa40214c13ad8b52e8e9ffc810f73aca
hakrimryu <hakrimryu@gmail.com>
2021-11-03 00:52:48 +0900

    モックアップ

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
 	urls := []string{
 		"https://google.com/",
 		"https://naver.com/",
 		"https://wrongaddress.com/",
 	}
+
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Alive! : ", url)
+
+	response, err := http.Get(url)
+
+	if err == nil || response.StatusCode >= 400 {
+		fmt.Println("Dead! : ", url)
+		return errRequestFailed
+	}
+
+	return nil
 }
